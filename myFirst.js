5e8a24ac06befadf3cc435c1c3bcd5cb54cba636
Andres Ruiz <andrsruiz@gmail.com>
2020-06-19 11:14:51 -0500

    Read Files

--- myFirst.js
+++ myFirst.js
@@ -1,13 +1,19 @@
 var http = require('http');
 var url = require('url');
+var filesystem = require('fs');
 var dt = require('./myFirstModule.js');
 
 http.createServer(function (req, res){
-	res.writeHead(200, {'Content-Type':'text/html'});
-	res.write("The current date is: " + dt.myDateTime());
-	res.write("<hr>");
-	// Enter http://localhost:8080/?year=2020&month=June
-	var q = url.parse(req.url, true).query;
-	var text = q.year + " " + q.month;
-	res.end(text);
+	filesystem.readFile('template.html', (error, data) => {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.write("The current date is: " + dt.myDateTime());
+		res.write("<hr>");
+		// Enter http://localhost:8080/?year=2020&month=June
+		var q = url.parse(req.url, true).query;
+		var text = q.year + " " + q.month;
+		res.write(text);
+		res.write("<hr>");
+		res.write(data)
+		return res.end();
+	});
 }).listen(8080);
