7986c9c7bd9175bcb3c8c6ec64c58aa778812bff
Andres Ruiz <andrsruiz@gmail.com>
2020-06-19 11:02:11 -0500

    Using url module

--- myFirst.js
+++ myFirst.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var url = require('url');
 var dt = require('./myFirstModule.js');
 
 http.createServer(function (req, res){
 	res.writeHead(200, {'Content-Type':'text/html'});
 	res.write("The current date is: " + dt.myDateTime());
-	res.end();
+	res.write("<hr>");
+	// Enter http://localhost:8080/?year=2020&month=June
+	var q = url.parse(req.url, true).query;
+	var text = q.year + " " + q.month;
+	res.end(text);
 }).listen(8080);
